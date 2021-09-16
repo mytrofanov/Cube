011ba6d477a64140d596915290fa7fe9b1ad2f10
mytrofanov <mytrofanovmaxym@gmail.com>
2021-09-16 21:13:40 +0300

    сделал функцию последовательного вывода информации о себе

--- js/main.js
+++ js/main.js
@@ -4,9 +4,15 @@ let aboutMeItems = 'Married, two kids'
 function animateText(id, text, i) {
   document.getElementById(id).innerHTML = text.substring(0, i);
   i++;
-
+  if (text!=aboutMeItems) {
+    if (i>text.length) {
+      id='about_me_items', text=aboutMeItems, i=0
+    }
+  }
   setTimeout("animateText('" + id + "','" + text + "'," + i + ")", 300);
 }
 
-animateText("about_me", text, 0);
-animateText('about_me_items', aboutMeItems, 0)
+animateText("about_me", text,0);
+
+
+
