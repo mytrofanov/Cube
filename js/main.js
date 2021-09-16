8fb837e8a09344b8ef11b26aa78edb45df73b581
mytrofanov <mytrofanovmaxym@gmail.com>
2021-09-16 19:39:33 +0300

    Добрался до фона

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-let text = 'Junior Frontend Developer'
+let text = 'Im an enthusiastic and detail-oriented Frontend Software Engineer' +
+  ' seeking an entry-level position with Company to use my skills in coding, ' +
+  'troubleshooting complex problems, and assisting in the timely completion of projects.'
 
 function animateText(id, text, i) {
   document.getElementById(id).innerHTML = text.substring(0, i);
   i++;
-  if (i > text.length) i = 0;
+
   setTimeout("animateText('" + id + "','" + text + "'," + i + ")", 100);
 }
 
-animateText('dynamic-text', text, 0  );
+
